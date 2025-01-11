c9c13eef88229822b2197447611cf87b97cc5244
enzzo <enzzomatrix@gmail.com>
2025-01-11 18:15:19 +1100

    final

---
+++ src/test/java/ru/vasilev/hellospringboot/HelloSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vasilev.hellospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
